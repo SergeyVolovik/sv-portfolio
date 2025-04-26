44b9b69394c08ea1c44b2405cad268981a9c96ab
SergeyVolovik <serhii.volovik@gmail.com>
2025-04-26 15:58:50 +0300

    bug/P-4 - hide the circle if link isn't included (#5)

--- src/hooks/useCircleNavLinkAnimation.tsx
+++ src/hooks/useCircleNavLinkAnimation.tsx
@@ -1,3 +1,4 @@
+import { NAVIGATION_LINKS_CONFIG } from '@/configs/NavigationLinks.config';
 import { useEffect, useRef, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
@@ -8,17 +9,28 @@ export const useCircleNavLinkAnimation = () => {
   const linksRef = useRef<Record<string, HTMLAnchorElement | null>>({});
   const [hovered, setHovered] = useState<string | null>(null);
   const activeLinkClassName = 'active__navlink';
+  const activeLink = linksRef.current[location.pathname];
 
   const moveCircleTo = (path: string) => {
     const link = linksRef.current[path];
     const circle = circleRef.current;
     const container = containerRef.current;
 
+    const isLinkIncluded = NAVIGATION_LINKS_CONFIG.some(
+      (link) => link.to === path
+    );
+
+    if (!link && !isLinkIncluded) {
+      circle?.style.setProperty('opacity', '0');
+      return;
+    } else {
+      circle?.style.setProperty('opacity', '1');
+    }
+
     if (link && circle && container) {
       const linkRect = link.getBoundingClientRect();
       const containerRect = container.getBoundingClientRect();
       const centerX = linkRect.left - containerRect.left + linkRect.width / 2;
-      const activeLink = linksRef.current[location.pathname];
 
       if (activeLink !== link) {
         activeLink?.classList.remove(activeLinkClassName);
