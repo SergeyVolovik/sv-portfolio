75328828b87ff423909598e16e7a6594437e4b24
SergeyVolovik <serhii.volovik@gmail.com>
2025-08-27 20:27:24 +0300

    story/22 - updated wrapper width; added badges, project title and description (#27)

--- src/components/layouts/Navigation.tsx
+++ src/components/layouts/Navigation.tsx
@@ -1,7 +1,7 @@
 import { ReactComponent } from '@/interfaces';
 
 export const Navigation = ({ children }: ReactComponent) => (
-  <nav className="navbar fixed w-full max-w-[590px] mt-[90px] p-0 px-[18px] justify-between bg-transparent gap-4 rounded-md border-none shadow-none z-50">
+  <nav className="navbar fixed w-full max-w-[620px] mt-[90px] p-0 px-[18px] justify-between bg-transparent gap-4 rounded-md border-none shadow-none z-50">
     {children}
   </nav>
 );
