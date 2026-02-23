4b0dddfe7f28b6b63be96d10cc538e665f42d944
Serhii Volovik <serhii.volovik@gmail.com>
2026-02-23 16:01:27 +0200

    story/P-24 - updated animation on the page loading/changing

--- src/components/layouts/Wrapper.tsx
+++ src/components/layouts/Wrapper.tsx
@@ -2,7 +2,7 @@ import { ReactComponent } from '@/interfaces';
 
 export const Wrapper = ({ children }: ReactComponent) => {
   return (
-    <section className="w-full min-h-screen max-w-[620px] mx-auto px-4 pt-[90px] pb-[25px] flex flex-col items-center justify-start gap-4">
+    <section className="w-full min-h-auto max-w-[620px] mx-auto px-4 pt-[90px] pb-[25px] flex flex-col items-center justify-start gap-4">
       {children}
     </section>
   );
